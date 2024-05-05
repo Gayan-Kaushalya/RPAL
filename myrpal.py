653741c19b470ee501b607274e5a1f0dbafe0c35
Gayan Kaushalya <gaykaush@gmail.com>
2024-05-05 16:15:15 +0530

    Added -st switch

--- myrpal.py
+++ myrpal.py
@@ -1,6 +1,7 @@
 # Description: This file is the main file that will be run to execute the program. It will take in the command line arguments and execute the program accordingly.
 # You cannot run this file in the IDE. You must run it in the terminal.
-
+# The command should be in the following format:
+# ./myrpal.py [-l] [-ast] [-st] filename
 
 from rpal_parser import parse
 from node import preorder_traversal
@@ -8,14 +9,11 @@ from standardizer import standardize
 from csemachine import *
 import sys
 
-
-
-
 if __name__ == "__main__":
     arguments = sys.argv
     
     if len(arguments) < 2:
-        print("Wrong command. Make sure the command is in the following format. \n ./myrpal.py [-l] [-ast] [-st] [-noout] filename")
+        print("Wrong command. Make sure the command is in the following format. \n ./myrpal.py [-l] [-ast] [-st] filename")
         sys.exit(1)
         
     else:
@@ -25,24 +23,31 @@ if __name__ == "__main__":
             
         else:
             switches = arguments[1 : -1]
+            file_name = arguments[-1]
+            
+            if "-l" in switches or "-ast" in switches or "-st" in switches:
 
-            # If '-l' is in the switches, we must print the file as it is.
-            if "-l" in switches:
-                file_name = arguments[-1]
-                
-                with open(file_name, "r") as file:
-                    print(file.read())
+                # If '-l' is in the switches, we must print the file as it is.
+                if "-l" in switches:
+                    with open(file_name, "r") as file:
+                        print(file.read())
+                        
+                    print()
                     
-                print()
+                # If '-ast' is in the switches, we must print the abstract syntax tree.
+                if "-ast" in switches:
+                    ast = parse(file_name)
+                    preorder_traversal(ast)
+                    
+                    print()
                 
-            # If '-ast' is in the switches, we must print the abstract syntax tree.
-            if "-ast" in switches:
-                file_name = arguments[-1]
-                ast = parse(file_name)
-                preorder_traversal(ast)
-                
-                print()
+                # If '-st' is in the switches, we must print the standardized tree.    
+                if "-st" in switches:
+                    st = standardize(file_name)  
+                    preorder_traversal(st)
+                    
+                    print()
             
             else:
-                print("Wrong command. Make sure the command is in the following format. \n ./myrpal.py [-l] [-ast] [-st] [-noout] filename")
+                print("Wrong command. Make sure the command is in the following format. \n ./myrpal.py [-l] [-ast] [-st] filename")
                 sys.exit(1)
